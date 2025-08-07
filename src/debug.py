96bb7a491f47358ec5f2988bb961c1ab2884118e
spacehare <dantath425@gmail.com>
2025-08-07 06:31:02 -0400

    Delete debug.py

--- src/debug.py
+++
@@ -1,8 +0,0 @@
-import app.settings
-from app.parse import parse
-from pathlib import Path
-
-file: Path = Path("I:\Quake\Dev\maps\_ignore\mpj2_yo\mapsrc\mpj2_yo.map")
-string_data: str = file.read_text()
-map_data = parse(string_data)
-print(map_data)
