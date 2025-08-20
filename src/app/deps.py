842d94d28d7bd83c7bbaa188bbddc9b472b7ee1d
spacehare <dantath425@gmail.com>
2025-08-20 15:34:20 -0400

    move pattern check to own class

--- src/app/deps.py
+++ src/app/deps.py
@@ -18,6 +18,11 @@ class GroupTypes(StrEnum):
     ALL = 'all'
 
 
+class GroupTags(StrEnum):
+    SAME_KEY = 'same_key'
+    IN_ENT = 'in_ent'
+
+
 @dataclass(kw_only=True)
 class Pattern:
     which: str = ''
@@ -67,10 +72,44 @@ class Pattern:
         return results
 
 
-# @dataclass
-# class PatternGroup:
-#     group_type: GroupTypes
-#     patterns: list[Pattern]
+@dataclass
+class Check:
+    c_type: GroupTypes
+    tags: list[GroupTags]
+    patterns: list[Pattern]
+
+    def check(self, ent: Entity) -> bool:
+        filter_keys: list[str] = []
+
+        for pattern in self.patterns:
+            keys = pattern.get_keys_from_entity(ent)
+            filter_keys.extend(keys)
+
+            print(keys)
+
+        match self.c_type:
+            case GroupTypes.ALL:
+                pass
+            case GroupTypes.ANY:
+                pass
+
+        # tags = check.get('tags')
+        # if tags:
+        #     # same_key
+        #     same_key = filter_keys.count(filter_keys[0]) == len(filter_keys)
+
+        #     # in_ent
+        #     in_ent = len(filter_keys) == len(group_patterns)
+
+        return False
+
+    @staticmethod
+    def from_dict(d: dict) -> 'Check':
+        return Check(
+            c_type=GroupTypes(d['type']),
+            tags=[GroupTags(t) for t in d['tags']],
+            patterns=[Pattern(**p['pattern']) for p in d['patterns']]
+        )
 
 
 @dataclass(kw_only=True)
@@ -79,25 +118,21 @@ class Master:
     name: str
     destination: str
     output_patterns: list[Pattern]
-    checks: list | None = None
+    checks: list[Check] | None = None
 
     @staticmethod
-    def from_dict(d: dict):
-        checks: list = d.get('checks', [])
+    def from_dict(d: dict) -> 'Master':
+        checks = [Check.from_dict(c) for c in d.get('checks', [])]
         name = d.get('name', 'unnamed pattern')
         dest: str = d['destination']
         outputs = [Pattern(**p['pattern']) for p in d['outputs']]
 
-        if not any([checks, name, outputs]):
-            print('could not create Master from dict')
-            return
-        else:
-            return Master(
-                name=name,
-                destination=dest,
-                output_patterns=outputs,
-                checks=checks,
-            )
+        return Master(
+            name=name,
+            destination=dest,
+            output_patterns=outputs,
+            checks=checks,
+        )
 
     def get_keys_from_entity(self, ent: Entity, depth=0) -> list[str] | None:
         print('master:', self.name)
@@ -107,34 +142,21 @@ class Master:
         if not self.output_patterns:
             print('master pattern needs at least one "output" pattern')
 
-        for pattern in self.output_patterns:
-            print(pattern)
-            output_keys.extend(pattern.get_keys_from_entity(ent))
-
         if self.checks:
             print('found checks')
             results: list[bool] = []
 
             for check in self.checks:
-                filter_keys: list[str] = []
-
-                group_type: str = check['type']
-                group_tags: list[str] = check['tags']
-                group_patterns: list[Pattern] = [Pattern(**p['pattern']) for p in check['patterns']]
-
-                for pattern in group_patterns:
-                    keys = pattern.get_keys_from_entity(ent)
-                    filter_keys.extend(keys)
-                    print(keys)
-
-                match group_type:
-                    case GroupTypes.ALL:
-                        pass
-                    case GroupTypes.ANY:
-                        pass
+                v = check.check(ent)
 
             # if the checks fail
             if not all(results):
                 return None
+
+        # find outputs
+        for pattern in self.output_patterns:
+            print(pattern)
+            keys = pattern.get_keys_from_entity(ent)
+            output_keys.extend(keys)
 
         return output_keys
