040405b993dcf84603922d91c6dc00309509dc23
spacehare <dantath425@gmail.com>
2025-08-20 10:46:06 -0400

    move dependency-finding functions into their classes

--- src/app/deps.py
+++ src/app/deps.py
@@ -26,8 +26,47 @@ class Pattern:
     condition: Conditions | str
     what: str | list[str] = ''
 
-    def match(self, key: str, value: str):
-        return match_pattern(key, value, self)
+    def get_truth_from_pair(self, key: str, value: str) -> bool:
+        '''for a single key-value pair, check against a pattern'''
+        # check if we are matching against a key or a value
+        which = None
+        match self.which:
+            case 'key':
+                which = key
+            case 'value':
+                which = value
+            case _:
+                return False
+
+        # check if string
+        if isinstance(self.what, str):
+            match self.condition:
+                case Conditions.STARTSWITH:
+                    return which.startswith(self.what)
+                case Conditions.ENDSWITH:
+                    return which.endswith(self.what)
+                case Conditions.EQUALS:
+                    return which == self.what
+
+        # check if list
+        elif isinstance(self.what, list):
+            match self.condition:
+                case Conditions.STARTSWITH:
+                    return which.startswith(tuple(self.what))
+                case Conditions.ENDSWITH:
+                    return which.endswith(tuple(self.what))
+                case Conditions.EQUALS:
+                    return which in self.what
+
+        return False
+
+    def get_keys_from_entity(self, ent: Entity) -> list[str]:
+        '''for a whole entity, find a list of matching keys'''
+        results: list[str] = []
+        for key, value in ent.kv.items():
+            if self.get_truth_from_pair(key, value):
+                results.append(key)
+        return results
 
 
 @dataclass(kw_only=True)
@@ -36,61 +75,23 @@ class Master:
     destination: str
     checks: dict
 
+    def get_keys_from_entity(self, ent: Entity, depth=0) -> list[str] | None:
+        print('master:', self.name)
 
-def match_pattern(key: str, value: str, pattern: Pattern) -> bool:
-    '''for a single key-value pair, check against a pattern'''
+        matches = []
+        output_dict: dict | None = None
+        output_pattern: Pattern | None = None
+        # output_result_key: str | None = None
 
-    # check if we are matching against a key or a value
-    which = None
-    match pattern.which:
-        case 'key':
-            which = key
-        case 'value':
-            which = value
-        case _:
-            return False
+        output_dict = self.checks.get('output')
+        if output_dict:
+            output_pattern = Pattern(**output_dict.get('pattern'))
 
-    # check if string
-    if isinstance(pattern.what, str):
-        match pattern.condition:
-            case Conditions.STARTSWITH:
-                return which.startswith(pattern.what)
-            case Conditions.ENDSWITH:
-                return which.endswith(pattern.what)
-            case Conditions.EQUALS:
-                return which == pattern.what
+        if not output_pattern:
+            print('each master pattern must have an "output" pattern')
+            return
 
-    # check if list
-    elif isinstance(pattern.what, list):
-        match pattern.condition:
-            case Conditions.STARTSWITH:
-                return which.startswith(tuple(pattern.what))
-            case Conditions.ENDSWITH:
-                return which.endswith(tuple(pattern.what))
-            case Conditions.EQUALS:
-                return which in pattern.what
+        keys = output_pattern.get_keys_from_entity(ent)
 
-    return False
-
-
-def get_key(ent: Entity, master: Master, depth=0) -> str | None:
-    matches = []
-    output_result = None
-    output_dict = None
-    output_pattern: Pattern | None = None
-
-    print('master:', master.name)
-    output_dict = master.checks.get('output')
-    if output_dict:
-        output_pattern = Pattern(**output_dict.get('pattern'))
-
-    if not output_pattern:
-        print('each master pattern must have an "output" pattern')
-        return
-
-    for key, value in ent.kv.items():
-        is_match = match_pattern(key, value, output_pattern)
-        if is_match:
-            output_result = key
-
-    return output_result
+        return keys
+        # return output_result_key
