f3f3dd596f33ad81bb295f89786d4820a7d3547e
spacehare <dantath425@gmail.com>
2025-09-19 02:30:05 -0400

    make the console output prettier

--- src/symlink.py
+++ src/symlink.py
@@ -1,5 +1,6 @@
 from .app import settings
 from pathlib import Path
+from .app.bcolors import bcolors, colorize
 import argparse
 
 
@@ -12,16 +13,20 @@ if __name__ == '__main__':
     dev_folder: Path = args.dev_folder
     mod_folder: Path = args.mod_folder
 
+    print('creating symlinks')
     for item in settings.symlinks:
         for path in dev_folder.rglob(item.target):
-            print('target', path)
-            print('destination', item.destination)
-
             new_path: Path = Path(mod_folder / item.destination / path.name)
 
-            print('link', new_path)
-            print('-----')
-            new_path.symlink_to(
-                target=path,
-                target_is_directory=path.is_dir()
-            )
+            print(colorize(path, bcolors.UNDERLINE))
+            print('\ttarget =', colorize(path, bcolors.OKBLUE))
+            print('\tdestination =', colorize(item.destination, bcolors.OKBLUE))
+            print('\tlink =', colorize(new_path, bcolors.OKBLUE))
+
+            if new_path.exists():
+                print(colorize(f"\t\"{new_path}\" already exists, skipping.", bcolors.WARNING))
+            else:
+                new_path.symlink_to(
+                    target=path,
+                    target_is_directory=path.is_dir()
+                )
