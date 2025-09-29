474596db094847e031247bfbff8a01f702096d73
spacehare <dantath425@gmail.com>
2025-09-29 02:30:47 -0400

    create new folder instead of dumping files into arg folder

--- rabbitquake/jamgen.py
+++ rabbitquake/jamgen.py
@@ -2,18 +2,18 @@ import argparse
 import shutil
 from pathlib import Path
 
-import rabbitquake.app.bcolors as bcolors
 import rabbitquake.app.settings as settings
 
 
-def gen(new_folder: Path, stem: str, create: bool = True) -> None:
-    if not new_folder.exists() and create:
-        new_folder.mkdir()
+def gen(parent_folder: Path, stem: str, create: bool = True) -> None:
+    new_parent = parent_folder / stem
+    if not new_parent.exists() and create:
+        new_parent.mkdir()
 
     print("-- create --")
     for item in settings.template.touch:
         item_path: Path = Path(item.replace("{mapstem}", stem))
-        path: Path = new_folder / item_path
+        path: Path = new_parent / item_path
         print(f"\t-> {path}")
 
         if create:
@@ -24,7 +24,7 @@ def gen(new_folder: Path, stem: str, create: bool = True) -> None:
 
     print("-- copy --")
     for pair in settings.template.copy_pairs:
-        dest = new_folder / str(pair.destination).replace("{mapstem}", stem)
+        dest = new_parent / str(pair.destination).replace("{mapstem}", stem)
         print(f"\t-> {dest}")
 
         if create:
@@ -33,7 +33,7 @@ def gen(new_folder: Path, stem: str, create: bool = True) -> None:
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("new_folder", type=Path, help="the new folder's path")
+    parser.add_argument("parent_folder", type=Path, help="the folder to put the new folder into.")
     parser.add_argument(
         "stem", type=str, help=f"the stem of the map. ex: qbj_rabbit, rm_myopia"
     )
@@ -45,4 +45,4 @@ if __name__ == "__main__":
     if args.fake:
         print("debug run; not creating or copying files.")
 
-    gen(args.new_folder, args.stem, not args.fake)
+    gen(args.parent_folder, args.stem, not args.fake)
