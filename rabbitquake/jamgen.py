b35765c5963af26f95e250d0ad8ccc28c220fa90
spacehare <dantath425@gmail.com>
2025-10-20 05:22:29 -0400

    simplify args

--- rabbitquake/jamgen.py
+++ rabbitquake/jamgen.py
@@ -5,15 +5,14 @@ from pathlib import Path
 import rabbitquake.app.settings as settings
 
 
-def gen(parent_folder: Path, stem: str, create: bool = True) -> None:
-    new_parent = parent_folder / stem
-    if not new_parent.exists() and create:
-        new_parent.mkdir()
+def gen(target_folder: Path, create: bool = True) -> None:
+    if not target_folder.exists() and create:
+        target_folder.mkdir()
 
     print("-- create --")
     for item in settings.template.touch:
-        item_path: Path = Path(item.replace("{mapstem}", stem))
-        path: Path = new_parent / item_path
+        item_path: Path = Path(item.replace("{mapstem}", target_folder.stem))
+        path: Path = target_folder / item_path
         print(f"\t-> {path}")
 
         if create:
@@ -24,7 +23,9 @@ def gen(parent_folder: Path, stem: str, create: bool = True) -> None:
 
     print("-- copy --")
     for pair in settings.template.copy_pairs:
-        dest = new_parent / str(pair.destination).replace("{mapstem}", stem)
+        dest = target_folder / str(pair.destination).replace(
+            "{mapstem}", target_folder.stem
+        )
         print(f"\t-> {dest}")
 
         if create:
@@ -33,9 +34,8 @@ def gen(parent_folder: Path, stem: str, create: bool = True) -> None:
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("parent_folder", type=Path, help="the folder to put the new folder into.")
     parser.add_argument(
-        "stem", type=str, help=f"the stem of the map. ex: qbj_rabbit, rm_myopia"
+        "parent_folder", type=Path, help="the folder to put all the new files into."
     )
     parser.add_argument(
         "--fake", action="store_true", help=f"don't actually copy or create any files."
@@ -45,4 +45,4 @@ if __name__ == "__main__":
     if args.fake:
         print("debug run; not creating or copying files.")
 
-    gen(args.parent_folder, args.stem, not args.fake)
+    gen(args.parent_folder, not args.fake)
