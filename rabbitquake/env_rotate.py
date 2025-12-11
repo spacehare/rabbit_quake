618d5079d64d4dbbc3064b385a8303c82a7175b2
spacehare <dantath425@gmail.com>
2025-12-11 18:37:52 -0500

    inverse the opposite face's transposition

--- rabbitquake/env_rotate.py
+++ rabbitquake/env_rotate.py
@@ -24,11 +24,13 @@ def rotate_image(image: Image.Image, turns: int) -> Image.Image:
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("parent_folder", type=Path)
+    parser.add_argument("-o", "--output_folder", type=Path)
     parser.add_argument("stem_without_direction", type=str)
     parser.add_argument("turns", type=int)
     args = parser.parse_args()
 
     parent_folder: Path = args.parent_folder
+    output_folder: Path | None = args.output_folder
     stem_body: str = args.stem_without_direction
     turns: int = args.turns
 
@@ -36,7 +38,6 @@ if __name__ == "__main__":
         raise ValueError("turns must be between 1 and 3")
 
     # rotate yaw
-    rotate_me = ["up", "dn"]
     dir_dict: dict[str, int] = {}
     faces = ["ft", "rt", "bk", "lf"]
     items = deque(faces)
@@ -49,16 +50,24 @@ if __name__ == "__main__":
             new_direction: str
 
             with Image.open(path) as img:
-                if direction in rotate_me:
+                if direction == "up":
                     new_image = rotate_image(img, turns)
+                    new_direction = direction
+                elif direction == "dn":
+                    reverse = {
+                        3: 1,
+                        2: 2,
+                        1: 3,
+                    }
+                    new_image = rotate_image(img, reverse[turns])
                     new_direction = direction
                 else:
                     dir_dict[direction] = items.index(direction)  # ex: lf = 2
                     new_image = img
                     new_direction = faces[dir_dict[direction]]
-                new_filename: Path = path.with_stem(
-                    f"{path.stem[:-2]}_{turns}_{new_direction}"
-                )
+                new_filename: Path = path.with_stem(f"{path.stem[:-2]}_{turns}_{new_direction}")
+                if output_folder:
+                    new_filename = output_folder / new_filename.name
                 new_image.save(new_filename)
 
             print("->", direction, new_direction, path)
